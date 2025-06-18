79fda598106923a6411d3d6ad023cdd19ebd49d0
Hitesh11112 <hiteshpatwal2004@gmail.com>
2025-06-18 21:53:57 +0530

    dynamically adding 2no

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-"" 
+const input1Elem = document.querySelector(".input1");
+const input2Elem = document.querySelector(".input2");
+const outputElem = document.querySelector(".output");
+
+
+const checkBtn = document.querySelector(".check").addEventListener('click',()=>{
+    outputElem.value = parseInt(input1Elem.value) + parseInt(input2Elem.value);
+});
+
+
